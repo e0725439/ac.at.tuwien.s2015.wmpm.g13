6373e9d29d97ba9de818520076e7a5d781edf782
Florin Bogdan Balint <e0725439@student.tuwien.ac.at>
2015-05-04 18:03:04 +0200

    Jetty hello world added

--- ac.at.tuwien.s2015.wmpm.g13/src/ac/at/tuwien/s2015/wmpm/g13/main/Main.java
+++ ac.at.tuwien.s2015.wmpm.g13/src/ac/at/tuwien/s2015/wmpm/g13/main/Main.java
@@ -8,6 +8,15 @@
  */
 package ac.at.tuwien.s2015.wmpm.g13.main;
 
+/**
+* Copyright (c) 2015. All rights reserved.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS".
+* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+* 
+*/
+import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +27,16 @@ public class Main {
 	public static void main(String[] args) {
 		LOGGER.debug("Program starting...");
 		
-		LOGGER.debug("Do something...");
+		Server server = new Server(8080);
+		
+		server.setHandler(new HelloPowerMaterials());
+ 
+        try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			LOGGER.error("Could not start server.", e);
+		}
 		
 		LOGGER.debug("Program ended...");
 	}
