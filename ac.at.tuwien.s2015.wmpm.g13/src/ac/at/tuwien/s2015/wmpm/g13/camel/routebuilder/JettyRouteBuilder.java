15f14b7fc5792ba3d8923dc00ec68b6b45aabbcc
Florin Bogdan Balint <e0725439@student.tuwien.ac.at>
2015-05-09 20:45:06 +0200

    #2 - branch created and JettyRouteBuilder added

---
+++ ac.at.tuwien.s2015.wmpm.g13/src/ac/at/tuwien/s2015/wmpm/g13/camel/routebuilder/JettyRouteBuilder.java
@@ -0,0 +1,19 @@
+package ac.at.tuwien.s2015.wmpm.g13.camel.routebuilder;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+public class JettyRouteBuilder extends RouteBuilder {
+	public void configure() {
+		from("jetty:http://localhost:8181/mytestservice").process(
+				new Processor() {
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						String message = exchange.getIn().getBody(String.class);
+						System.out.println("Hello Mr :" + message);
+						exchange.getOut().setBody("Hello world Mr " + message);
+					}
+				});
+	}
+}
